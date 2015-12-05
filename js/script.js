e3ef5acfdddad458b98cb9d3ce75e99c06a6820a
Chris Donofrio <cdonofrio20@gmail.com>
2015-12-05 13:34:31 -0500

    Added google map API key

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
   $(".btn-default").on("click", function(e){
     e.preventDefault();
+
+
+	var userAddress = $("#userAddress").val();
+    var googleApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?";
+    googleApiUrl += "key=AIzaSyCLXwXnb5FasH9MVlYx0rRSzSqNkvRMRQI";
+    googleApiUrl += "&address=" + userAddress;
+
+
     alert("hello");
   });
+
+
+
 });
